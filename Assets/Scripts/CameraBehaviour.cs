f200efa2c6d752027f11a467f507ae9056b4669c
aiv.manuel.martignon <aiv.manuel.martignon@gmail.com>
2020-11-30 23:47:17 +0100

    Basic system is stable

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum CameraExecutionMode { Undefined, Immediate, ConstantLerp, NormalLerp, SmoothLerp }
-public enum CameraInPlaceBehaviour { Undefined, Normal, SteadyCam }
 
 public class CameraBehaviour : MonoBehaviour
 {
+    [HideInInspector] public Transform Camera; //Camera transform (must be child of this gameobject!)
+    public bool UsingSteadyCam; //is the camera using the SteadyCam simulation?
+    public bool Moving; //is the Camera moving?
+    public bool Rotating; //is the Camera rotating?
+
     public Transform TEST_POSITION_TARGET;
     public Transform TEST_ROTATION_TARGET;
 
-    public CameraExecutionMode CameraPositioningMode = CameraExecutionMode.Undefined;
-    public CameraExecutionMode CameraRotatingMode = CameraExecutionMode.Undefined;
-    public CameraInPlaceBehaviour CameraInPlaceBehaviour = CameraInPlaceBehaviour.Undefined;
+    private Coroutine SteadyCamCoroutine;
+
+    public CameraExecutionMode CameraPositioningMode; //how will the camera move from A pos to B pos
+    public CameraExecutionMode CameraRotatingMode; //how will the camera rotate from A rot to B rot
 
     //Normal Lerp
     public float PositionLerpSpeed;
@@ -30,8 +35,17 @@ public class CameraBehaviour : MonoBehaviour
     [HideInInspector] public Vector3 TargetPosition;
     [HideInInspector] public Quaternion TargetRotation;
 
+    public float SteadyCamLerpSpeed;
+
     private void Awake()
     {
+        if (CameraPositioningMode == CameraExecutionMode.Undefined)
+            CameraPositioningMode = CameraExecutionMode.Immediate;
+        if (CameraRotatingMode == CameraExecutionMode.Undefined)
+            CameraRotatingMode = CameraExecutionMode.Immediate;
+
+        Camera = transform.GetChild(0);
+
         CameraEventSystem.SetCameraTargetPositionEvent += SetCameraTargetPosition;
         CameraEventSystem.SetCameraTargetRotationEvent += SetCameraTargetRotation;
         CameraEventSystem.CameraTargetPositionReachedEvent += CameraTargetPositionReached;
@@ -49,7 +63,13 @@ public class CameraBehaviour : MonoBehaviour
 
     private void SetCameraTargetPosition(Vector3 position)
     {
+        Moving = true;
         TargetPosition = position;
+        if (SteadyCamCoroutine != null)
+        {
+            StopCoroutine(SteadyCamCoroutine);
+            StartCoroutine(RecenterCamera());
+        }
 
         switch (CameraPositioningMode)
         {
@@ -64,7 +84,7 @@ public class CameraBehaviour : MonoBehaviour
                 StartCoroutine(ConstantLerpPositioning());
                 break;
             case CameraExecutionMode.NormalLerp:
-                StartCoroutine(VariableLerpPositioning());
+                StartCoroutine(NormalLerpPositioning());
                 break;
             case CameraExecutionMode.SmoothLerp:
                 StartCoroutine(SmoothLerpPositioning());
@@ -74,7 +94,14 @@ public class CameraBehaviour : MonoBehaviour
 
     private void SetCameraTargetRotation(Quaternion rotation)
     {
+        Rotating = true;
         TargetRotation = rotation;
+
+        if (SteadyCamCoroutine != null)
+        {
+            StopCoroutine(SteadyCamCoroutine);
+            StartCoroutine(RecenterCamera());
+        }
 
         switch (CameraRotatingMode)
         {
@@ -89,7 +116,7 @@ public class CameraBehaviour : MonoBehaviour
                 StartCoroutine(ConstantLerpRotating());
                 break;
             case CameraExecutionMode.NormalLerp:
-                StartCoroutine(VariableLerpRotating());
+                StartCoroutine(NormalLerpRotating());
                 break;
             case CameraExecutionMode.SmoothLerp:
                 StartCoroutine(SmoothLerpRotating());
@@ -97,51 +124,75 @@ public class CameraBehaviour : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// The camera has reached the target position.
+    /// </summary>
     private void CameraTargetPositionReached()
     {
+        Moving = false;
         Debug.Log("POSITION REACHED");
+        if (!Rotating)
+            ActivateSteadyCam();
     }
 
+    /// <summary>
+    /// The camera has reached the target rotation.
+    /// </summary>
     private void CameraTargetRotationReached()
     {
+        Rotating = false;
         Debug.Log("ROTATION REACHED");
+        if (!Moving)
+            ActivateSteadyCam();
     }
 
+    /// <summary>
+    /// Move the camera towards the destination at constant speed, reaching in the given time.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator ConstantLerpPositioning()
     {
         ElapsedPositioningLerpTime = 0;
-        Vector3 StartingPosition = transform.position;
+        Vector3 startingPosition = transform.position;
         float step;
         while (ElapsedPositioningLerpTime < PositionDuration)
         {
             step = ElapsedPositioningLerpTime / PositionDuration;
             ElapsedPositioningLerpTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(StartingPosition, TargetPosition, step);
+            transform.position = Vector3.Lerp(startingPosition, TargetPosition, step);
             yield return null;
         }
 
         CameraEventSystem.CameraTargetPositionReached();
     }
 
+    /// <summary>
+    /// Rotate the camera towards the destination rotation at constant speed, reaching in the given time.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator ConstantLerpRotating()
     {
         ElapsedRotatingLerpTime = 0;
-        Quaternion StartingRotation = transform.rotation;
+        Quaternion startingRotation = transform.rotation;
         float step;
         while (ElapsedRotatingLerpTime < RotationDuration)
         {
             step = ElapsedRotatingLerpTime / RotationDuration;
             ElapsedRotatingLerpTime += Time.deltaTime;
-            transform.rotation = Quaternion.Slerp(StartingRotation, TargetRotation, step);
+            transform.rotation = Quaternion.Slerp(startingRotation, TargetRotation, step);
             yield return null;
         }
 
         CameraEventSystem.CameraTargetRotationReached();
     }
 
-    private IEnumerator VariableLerpPositioning()
+    /// <summary>
+    /// Move the camera towards the destination, slowing down while approaching.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator NormalLerpPositioning()
     {
-        while (Vector3.Distance(transform.position, TargetPosition) > 0.5f)
+        while (Vector3.Distance(transform.position, TargetPosition) > 0.2f)
         {
             transform.position = Vector3.Lerp(transform.position, TargetPosition, PositionLerpSpeed * Time.deltaTime);
             yield return null;
@@ -150,7 +201,11 @@ public class CameraBehaviour : MonoBehaviour
         CameraEventSystem.CameraTargetPositionReached();
     }
 
-    private IEnumerator VariableLerpRotating()
+    /// <summary>
+    /// Rotate the camera towards the destination rotation, slowing down while reaching it.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator NormalLerpRotating()
     {
         while (Quaternion.Angle(transform.rotation, TargetRotation) > 5)
         {
@@ -161,37 +216,129 @@ public class CameraBehaviour : MonoBehaviour
         CameraEventSystem.CameraTargetRotationReached();
     }
 
+    /// <summary>
+    /// Move the camera towards the destination, starting slow, accelerating, decelerating near the end, reaching in the given time.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator SmoothLerpPositioning()
     {
         ElapsedPositioningLerpTime = 0;
-        Vector3 StartingPosition = transform.position;
+        Vector3 startingPosition = transform.position;
         float step;
         while (ElapsedPositioningLerpTime < PositionDuration)
         {
             step = ElapsedPositioningLerpTime / PositionDuration;
             step = step * step * (3f - 2f * step);
             ElapsedPositioningLerpTime += Time.deltaTime;
-            transform.position = Vector3.Lerp(StartingPosition, TargetPosition, step);
+            transform.position = Vector3.Lerp(startingPosition, TargetPosition, step);
             yield return null;
         }
 
         CameraEventSystem.CameraTargetPositionReached();
     }
 
+    /// <summary>
+    /// Rotate the camera towards the destination rotation, starting slow, accelerating, decelerating near the end, reaching in the given time.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator SmoothLerpRotating()
     {
         ElapsedRotatingLerpTime = 0;
-        Quaternion StartingRotation = transform.rotation;
+        Quaternion startingRotation = transform.rotation;
         float step;
         while (ElapsedRotatingLerpTime < RotationDuration)
         {
             step = ElapsedRotatingLerpTime / RotationDuration;
             step = step * step * (3f - 2f * step);
             ElapsedRotatingLerpTime += Time.deltaTime;
-            transform.rotation = Quaternion.Slerp(StartingRotation, TargetRotation, step);
+            transform.rotation = Quaternion.Slerp(startingRotation, TargetRotation, step);
             yield return null;
         }
 
         CameraEventSystem.CameraTargetRotationReached();
+    }
+
+    public void ActivateSteadyCam()
+    {
+        UsingSteadyCam = true;
+        SteadyCamCoroutine = StartCoroutine(SimulateSteadyCam());
+    }
+
+    /// <summary>
+    /// Simulate the SteadyCam movement on the camera.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator SimulateSteadyCam()
+    {
+        float steadyLerpSpeed = 0.5f;
+        Vector3 randomPos;
+        Vector3 destinationPosition;
+        Vector3 startingPosition;
+        float step;
+        float duration;
+        float elapsedSteadyTimeElapsed;
+        int xSign = 0;
+        int ySign = 0;
+        while (true)
+        {
+            duration = Random.Range(3.5f, 4.5f);
+            elapsedSteadyTimeElapsed = 0;
+            randomPos = Random.insideUnitCircle;// * 0.5f;
+
+            //Debug.Log("PRIMA ||| Segno X: " + xSign + "Segno Y: " + ySign);
+            if ((int)Mathf.Sign(randomPos.x) == xSign)
+                randomPos.x = -randomPos.x;
+            if ((int)Mathf.Sign(randomPos.y) == ySign)
+                randomPos.y = -randomPos.y;
+
+            xSign = (int)Mathf.Sign(randomPos.x);
+            ySign = (int)Mathf.Sign(randomPos.y);
+
+            randomPos.x = Mathf.Clamp(randomPos.x, -0.1f, 0.1f);
+            randomPos.y = Mathf.Clamp(randomPos.y, -0.1f, 0.1f);
+
+            //Debug.Log("DOPO ||| Segno X: " + xSign + "Segno Y: " + ySign);
+            Debug.Log("New Random Pos: " + randomPos);
+
+            destinationPosition = transform.position + randomPos;
+            startingPosition = Camera.position;
+            while (elapsedSteadyTimeElapsed < duration)
+            {
+                step = elapsedSteadyTimeElapsed / duration;
+                step = step * step * (3f - 2f * step);
+                elapsedSteadyTimeElapsed += Time.deltaTime;
+                Camera.position = Vector3.Lerp(startingPosition, destinationPosition, step);
+                yield return null;
+            }
+
+            //while (Vector3.Distance(Camera.position, destinationPosition) > 0.1f)
+            //{
+            //    Camera.position = Vector3.Lerp(Camera.position, destinationPosition, steadyLerpSpeed * Time.deltaTime);
+            //    yield return null;
+            //}
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Move back the camera child to the central position to match this gameobject.
+    /// Useful if you are interrupting a SteadyCam movement for any reason.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator RecenterCamera()
+    {
+        float recenterDuration = 1f;
+        float recenterTimeElapsed = 0f;
+        float step;
+        Vector3 startingPos = Camera.position;
+        while (recenterTimeElapsed < recenterDuration)
+        {
+            step = recenterTimeElapsed / recenterDuration;
+            step = step * step * (3f - 2f * step);
+            recenterTimeElapsed += Time.deltaTime;
+            Camera.position = Vector3.Lerp(startingPos, transform.position, step);
+            yield return null;
+        }
+        UsingSteadyCam = false;
     }
 }
